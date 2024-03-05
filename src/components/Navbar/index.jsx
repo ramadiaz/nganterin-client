1f9b064bac63c36aae0d2894b983f857c814dd45
Rama Diaz <ramadiaz221@gmail.com>
2024-03-05 17:58:53 +0700

    Navbar update

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,200 +1,70 @@
 "use client";
 
+import { BellSimple } from "@phosphor-icons/react";
 import Image from "next/image";
 import Link from "next/link";
-import { Popover, Transition } from "@headlessui/react";
-import { ChevronDownIcon } from "@heroicons/react/20/solid";
-import { Fragment } from "react";
-
-const solutions = [
-  {
-    name: "Insights",
-    description: "Measure actions your users take",
-    href: "##",
-    icon: IconOne,
-  },
-  {
-    name: "Automations",
-    description: "Create your own targeted content",
-    href: "##",
-    icon: IconTwo,
-  },
-  {
-    name: "Reports",
-    description: "Keep track of your growth",
-    href: "##",
-    icon: IconThree,
-  },
-];
 
 const Navbar = () => {
   return (
-    <div className="flex flex-row border-b border-slate-200/30 h-16 px-8 items-center justify-between">
+    <div className="flex flex-row h-32 px-28 items-center justify-between">
       <div className="flex flex-row items-center gap-4">
-        <h2 className="font-semibold text-xl">
+        <Link href={`/`} className="font-semibold text-3xl mr-4">
           Nganterin
-        </h2>
+        </Link>
         <Link
-          href={`/flights`}
+          href={`/##`}
           className="hover:opacity-80 transition-all duration-500"
         >
           Flights
         </Link>
         <Link
-          href={`/hotels`}
+          href={`/##`}
           className="hover:opacity-80 transition-all duration-500"
         >
           Hotels
         </Link>
-
-        <div className="">
-          <Popover className="relative">
-            {({ open }) => (
-              <>
-                <Popover.Button className="group inline-flex items-center rounded-md px-3 py-2 hover:opacity-80 transition-all duration-500 ring-0">
-                  <span>Solutions</span>
-                  <ChevronDownIcon
-                    className="ml-2 h-5 w-5 transition-all duration-500 group-hover:opacity-80"
-                    aria-hidden="true"
-                  />
-                </Popover.Button>
-                <Transition
-                  as={Fragment}
-                  enter="transition ease-out duration-200"
-                  enterFrom="opacity-0 translate-y-1"
-                  enterTo="opacity-100 translate-y-0"
-                  leave="transition ease-in duration-150"
-                  leaveFrom="opacity-100 translate-y-0"
-                  leaveTo="opacity-0 translate-y-1"
-                >
-                  <Popover.Panel className="absolute z-10 mt-3 w-screen max-w-sm transform px-4 sm:px-0 lg:max-w-3xl">
-                    <div className="overflow-hidden rounded-lg shadow-lg">
-                      <div className="relative grid gap-8 bg-white p-7 lg:grid-cols-2">
-                        {solutions.map((item) => (
-                          <a
-                            key={item.name}
-                            href={item.href}
-                            className="-m-3 flex items-center rounded-lg p-2 transition duration-150 ease-in-out hover:bg-gray-50 focus:outline-none focus-visible:ring focus-visible:ring-orange-500/50"
-                          >
-                            <div className="flex h-10 w-10 shrink-0 items-center justify-center text-white sm:h-12 sm:w-12">
-                              <item.icon aria-hidden="true" />
-                            </div>
-                            <div className="ml-4">
-                              <p className="text-sm font-medium text-gray-900">
-                                {item.name}
-                              </p>
-                              <p className="text-sm text-gray-500">
-                                {item.description}
-                              </p>
-                            </div>
-                          </a>
-                        ))}
-                      </div>
-                      <div className="bg-gray-50 p-4">
-                        <a
-                          href="##"
-                          className="flow-root rounded-md px-2 py-2 transition duration-150 ease-in-out hover:bg-gray-100 focus:outline-none focus-visible:ring focus-visible:ring-orange-500/50"
-                        >
-                          <span className="flex items-center">
-                            <span className="text-sm font-medium text-gray-900">
-                              Documentation
-                            </span>
-                          </span>
-                          <span className="block text-sm text-gray-500">
-                            Start integrating products and tools
-                          </span>
-                        </a>
-                      </div>
-                    </div>
-                  </Popover.Panel>
-                </Transition>
-              </>
-            )}
-          </Popover>
-        </div>
       </div>
-      <div>fsdfsd</div>
+
+      <div className="flex flex-row items-center gap-4">
+        <Link
+          href={`/##`}
+          className="hover:opacity-80 transition-all duration-500 text-lg"
+        >
+          Home
+        </Link>
+        <Link
+          href={`/##`}
+          className="hover:opacity-80 transition-all duration-500 text-lg"
+        >
+          Why Nganterin
+        </Link>
+        <Link
+          href={`/##`}
+          className="hover:opacity-80 transition-all duration-500 text-lg"
+        >
+          Contact
+        </Link>
+        <Link
+          href={`/##`}
+          className="hover:opacity-80 transition-all duration-500 text-lg"
+        >
+          FAQ
+        </Link>
+      </div>
+      <div className="flex flex-row items-center gap-6">
+        <Link href={`/##`} className="border-2 rounded-full px-4 py-2 text-sm">
+          List your reservations
+          </Link>
+        <BellSimple size={28} color="#fcfcfc" />
+        <Image
+          src={`/avatar/default.png`}
+          width={45}
+          height={45}
+          className="rounded-full ml-8"
+        />
+      </div>
     </div>
   );
 };
-
-function IconOne() {
-  return (
-    <svg
-      width="48"
-      height="48"
-      viewBox="0 0 48 48"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect width="48" height="48" rx="8" fill="#FFEDD5" />
-      <path
-        d="M24 11L35.2583 17.5V30.5L24 37L12.7417 30.5V17.5L24 11Z"
-        stroke="#FB923C"
-        strokeWidth="2"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M16.7417 19.8094V28.1906L24 32.3812L31.2584 28.1906V19.8094L24 15.6188L16.7417 19.8094Z"
-        stroke="#FDBA74"
-        strokeWidth="2"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M20.7417 22.1196V25.882L24 27.7632L27.2584 25.882V22.1196L24 20.2384L20.7417 22.1196Z"
-        stroke="#FDBA74"
-        strokeWidth="2"
-      />
-    </svg>
-  );
-}
-
-function IconTwo() {
-  return (
-    <svg
-      width="48"
-      height="48"
-      viewBox="0 0 48 48"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect width="48" height="48" rx="8" fill="#FFEDD5" />
-      <path
-        d="M28.0413 20L23.9998 13L19.9585 20M32.0828 27.0001L36.1242 34H28.0415M19.9585 34H11.8755L15.9171 27"
-        stroke="#FB923C"
-        strokeWidth="2"
-      />
-      <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M18.804 30H29.1963L24.0001 21L18.804 30Z"
-        stroke="#FDBA74"
-        strokeWidth="2"
-      />
-    </svg>
-  );
-}
-
-function IconThree() {
-  return (
-    <svg
-      width="48"
-      height="48"
-      viewBox="0 0 48 48"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <rect width="48" height="48" rx="8" fill="#FFEDD5" />
-      <rect x="13" y="32" width="2" height="4" fill="#FDBA74" />
-      <rect x="17" y="28" width="2" height="8" fill="#FDBA74" />
-      <rect x="21" y="24" width="2" height="12" fill="#FDBA74" />
-      <rect x="25" y="20" width="2" height="16" fill="#FDBA74" />
-      <rect x="29" y="16" width="2" height="20" fill="#FB923C" />
-      <rect x="33" y="12" width="2" height="24" fill="#FB923C" />
-    </svg>
-  );
-}
 
 export default Navbar;
