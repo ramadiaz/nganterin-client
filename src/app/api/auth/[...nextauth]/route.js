c14cc19e049c59a457073666c6c0ed1aae467ac3
Rama Diaz <xanpoi221@gmail.com>
2024-05-20 07:36:03 +0700

    UPDATE of AuthProviders

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -7,11 +7,33 @@ const handler = NextAuth({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
       profile(profile) {
-        console.log({profile})
-        return
+        return {
+          id: profile.sub,
+          email: profile.email,
+          image: profile.picture,
+        };
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user }) {
+      // If a user is returned from the provider, add it to the token
+      if (user) {
+        token.id = user.id;
+        token.email = user.email;
+        token.image = user.image; // Add this line if you want to store the user's image
+      }
+      return token;
+    },
+
+    async session({ session, token }) {
+      // Save the token to the session to be used in the frontend
+      session.user.id = token.id;
+      session.user.email = token.email;
+      session.user.image = token.image; // Add this line if you want to store the user's image
+      return session;
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
