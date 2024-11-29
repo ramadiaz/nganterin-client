c1659187f06da1fe6f697fbea0cbde5080f1a0a7
Rama Diaz <ramadiaz221@gmail.com>
2024-11-29 16:06:56 +0700

    DELETE of unnecessary things

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -13,15 +13,12 @@ export const metadata = {
   description: "Flight and Hotel Reservations Services",
 };
 
-const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
-
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
         className={`${inter.className}, text-white bg-orange-50 min-h-screen`}
       >
-        <GoogleOAuthProvider clientId={CLIENT_ID}>
           <Providers>
             <div className="sticky top-0 w-full z-50">
               <NavbarComponent />
@@ -30,7 +27,6 @@ export default function RootLayout({ children }) {
             <Footer />
             <ToastContainer />
           </Providers>
-        </GoogleOAuthProvider>
       </body>
     </html>
   );
