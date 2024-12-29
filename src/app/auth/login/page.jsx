b71c0e18211df4fbd54c947c334c5c14169f9ea7
Rama Diaz <ramadiaz221@gmail.com>
2024-12-29 19:30:12 +0700

    INIT of AOS for /auth/login page

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -1,13 +1,19 @@
 "use client";
 
 import { Image } from "@nextui-org/react";
-
-import "aos/dist/aos.css";
 import { LogInContainer } from "@/components/LoginContainer";
+import { useEffect } from "react";
+
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Page = () => {
+  useEffect(() => {
+    AOS.init({ duration: 1200 });
+  }, []);
+
   return (
-    <div className="mx-auto max-w-5xl w-full mt-20">
+    <div className="mx-auto max-w-5xl w-full mt-20" data-aos="fade-up">
       <div className="flex flex-row">
         <div className="w-1/2 flex items-center justify-center h-auto">
           <Image
@@ -18,7 +24,7 @@ const Page = () => {
           />
         </div>
         <div className="w-1/2">
-          <LogInContainer/>
+          <LogInContainer />
         </div>
       </div>
 
