38617274b53e402e2c4a64afc2aff3f1fa82c010
Rama Diaz <ramadiaz221@gmail.com>
2025-01-12 11:50:36 +0700

    UPDATE of /payment/hotel mobile responsive

--- src/app/payment/hotel/page.jsx
+++ src/app/payment/hotel/page.jsx
@@ -68,18 +68,18 @@ const Page = () => {
 
     return (
         <div className={!orderData.id && "hidden"}>
-            <div className="w-max max-w-md mx-auto mt-8 text-gray-900">
+            <div className="w-full sm:w-max max-w-md mx-auto mt-8 text-gray-900 px-2">
                 <iframe
                     src={iframeData[paymentStatus]}
                     className="size-60 mx-auto"
                 ></iframe>
                 <h2 className="text-center text-xl font-bold mt-8">{headlineData[paymentStatus]}</h2>
-                <h3 className="text-center text-sm font-thin">Transaction Number: {orderData.id}</h3>
+                <h3 className="text-center text-sm font-thin whitespace-normal">Transaction Number: {orderData.id}</h3>
                 <div className="space-y-2 my-4">
-                    <Button className={`w-full ${paymentStatus === "paid" && "hidden"}`} variant="bordered" radius="full" onClick={() => fetchData(orderData.id)} isDisabled={isLoading}>Refresh <ArrowsClockwise size={22} color="#111827" weight="bold" className={isLoading && "animate-spinner-ease-spin"} /></Button>
-                    <div className="flex flex-row gap-2">
-                        <Button className="w-full bg-yellow-200 border-gray-900" variant="bordered" radius="full" as={Link} href="/order/history/hotel">My Orders <ClockCounterClockwise size={22} color="#111827" weight="bold" /></Button>
-                        <Button className="w-full bg-sky-300 border-gray-900" variant="bordered" radius="full" as={Link} href="/reservation/hotel">My Reservations <Ticket size={22} color="#111827" weight="bold" /></Button>
+                    <Button className={`w-full ${paymentStatus === "paid" && "hidden"}`} variant="bordered" radius="full" onClick={() => fetchData(orderData.id)} isDisabled={isLoading}>Refresh <ArrowsClockwise size={22} color="#fff" weight="bold" className={isLoading && "animate-spinner-ease-spin"} /></Button>
+                    <div className="flex flex-col sm:flex-row items-center gap-2">
+                        <Button className="w-full sm:w-1/2 bg-gradient-to-r from-amber-400 to-amber-600 text-white" as={Link} href="/order/history/hotel">My Orders <ClockCounterClockwise size={22} color="#fff" weight="bold" /></Button>
+                        <Button className="w-full sm:w-1/2 bg-gradient-to-r from-sky-500 to-sky-700 text-white" as={Link} href="/reservation/hotel">My Reservations <Ticket size={22} color="#fff" weight="bold" /></Button>
                     </div>
                 </div>
             </div>
